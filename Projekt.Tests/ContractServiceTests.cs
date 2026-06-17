c211d6b5f8f22e15427ceea5ea38e8bc6667baf2
Matlej44 <Matlej44@gmail.com>
2026-06-17 20:38:10 +0200

    Added basic tests for contractService

---
+++ Projekt.Tests/ContractServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Projekt.Data;
+using Projekt.DTOs.ContractDTOs;
+using Projekt.Exceptions;
+using Projekt.Services;
+
+namespace DefaultNamespace;
+
+public class ContractServiceTests
+{
+    private AppDbContext GetDbContext()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>().UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+        
+        return new AppDbContext(options);
+    }
+
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(4)]
+    public async Task CreateContractInvalidSupportYears_ThrowsBadRequestException(int supportYears)
+    {
+        await using var context = GetDbContext();
+        var service = new ContractService(context);
+        var dto = new AddContractDTO
+        {
+            ClientId = 1,
+            SoftwareId = 1,
+            AdditionalSupportYears = supportYears,
+            SoftwareVersion = "1.0.0"
+        };
+        await Assert.ThrowsAsync<BadRequestException>(() => service.CreateContract(dto));
+    }
+}

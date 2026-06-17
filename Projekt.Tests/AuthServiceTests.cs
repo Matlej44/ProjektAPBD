4c09547f8135ad2737e6fd45d3a78d665db57d52
Matlej44 <Matlej44@gmail.com>
2026-06-17 18:02:26 +0200

    Added unit tests for AuthService, including invalid login and valid login scenarios.

--- Projekt.Tests/AuthServiceTests.cs
+++ Projekt.Tests/AuthServiceTests.cs
@@ -42,4 +42,55 @@ public class AuthServiceTests
         };
         await Assert.ThrowsAsync<UnauthorizedException>(() => authService.Login(loginDto));
     }
+    [Fact]
+    public async Task Login_invalidLogin_ThrowsUnauthorizedException()
+    {
+        await using var context = GetDbContext();
+        var employee = new Employee
+        {
+            Login = "test",
+            PasswordHash = "correctHash",
+            Role = "admin"
+        };
+        await context.Employees.AddAsync(employee);
+        await context.SaveChangesAsync();
+        var mockHasher = new Mock<IPasswordHasher<Employee>>();
+        var mockConfig = new Mock<IConfiguration>();
+        mockHasher.Setup(x => x.VerifyHashedPassword(employee, employee.PasswordHash, "correctPassword")).Returns(PasswordVerificationResult.Success);
+        var authService = new AuthService(context, mockHasher.Object, mockConfig.Object);
+        var loginDto = new LoginDTO
+        {
+            Login = "wrongLogin",
+            Password = "correctPassword"
+        };
+        await Assert.ThrowsAsync<UnauthorizedException>(() => authService.Login(loginDto));
+    }
+
+    [Fact]
+    public async Task Login_validLogin_ReturnsTokenDTO()
+    {
+        await using var context = GetDbContext();
+        var employee = new Employee
+        {
+            Login = "test",
+            PasswordHash = "correctHash",
+            Role = "admin"
+        };
+        await context.Employees.AddAsync(employee);
+        await context.SaveChangesAsync();
+        var mockHasher = new Mock<IPasswordHasher<Employee>>();
+        var mockConfig = new Mock<IConfiguration>();
+        //Token do nowych algorytmów musi być co najmniej 128 bit
+        mockConfig.Setup(x => x["Jwt:Key"]).Returns("6198727cee8c2ddc5a96158094a12fdc");
+        mockConfig.Setup(x => x["Jwt:Issuer"]).Returns("testIssuer");
+        mockHasher.Setup(x => x.VerifyHashedPassword(employee, employee.PasswordHash, "correctPassword")).Returns(PasswordVerificationResult.Success);
+        var authService = new AuthService(context, mockHasher.Object, mockConfig.Object);
+        var loginDto = new LoginDTO
+        {
+            Login = "test",
+            Password = "correctPassword"
+        };
+        var token = await authService.Login(loginDto);
+        Assert.NotNull(token);
+    }
 }
